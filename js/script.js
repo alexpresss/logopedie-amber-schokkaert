10ae204e0bc357937bdf5fb2b2640ae423568f4e
Alexander Muylaert <alexander.muylaert@outlook.com>
2025-07-08 21:44:02 +0200

    update

--- js/script.js
+++ js/script.js
@@ -124,3 +124,10 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", () => {
   
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
